e4ee58846b5e1996574a316f18e6b6bf21e08d73
Master-Noiza <168280380+Master-Noiza@users.noreply.github.com>
2024-10-30 23:27:54 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Vilenought Wiki",
+    pageTitle: "Kompendium der Alten Welt",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
